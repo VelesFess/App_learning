4ca804db2f889e25a359a9d1a310a6a44f89309d
Konstantin <kmaklashov@mail.ru>
2024-09-30 17:32:14 +0300

    Understood how to test with docker started working.

--- src/tests/integration/test_users/test_api_users.py
+++ src/tests/integration/test_users/test_api_users.py
@@ -1,4 +1,8 @@
 # TODO: Реализовать тесты на API пользователей
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
 
 
 async def test_user_list(client, admin_token):
@@ -10,3 +14,13 @@ async def test_user_list(client, admin_token):
     )
     assert resp.status_code == 200
     assert len(resp.json()) == 1
+
+
+async def test_user_create(client, admin_token):
+    resp = await client.post("/users/")
+    assert resp.status_code == 403
+
+    resp = await client.get(
+        "/users/", headers={"Authorization": f"Bearer {admin_token}"}
+    )
+    assert resp.status_code == 200
